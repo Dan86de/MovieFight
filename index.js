22aa2f4f76c2bbcc95eeb6b6e8694f3e5a7143b1
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-08-27 15:41:03 +0200

    Added search logic

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there!');
+console.log("Hi there!");
+
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "802c1358",
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
+const input = document.querySelector("input");
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const onInput = (e) => {
+  fetchData(e.target.value);
+};
+
+input.addEventListener("input", debounce(onInput, 500));
