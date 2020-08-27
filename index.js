3859d638dc9573545144f60a49ac1f8ffcd4ed3b
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-08-27 16:34:40 +0200

    Added closing functionality
    Cool method with contains

--- index.js
+++ index.js
@@ -31,15 +31,17 @@ const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
+  resultsWrapper.innerHTML = ``;
 
   dropdown.classList.add("is-active");
 
   for (let movie of movies) {
     const option = document.createElement("a");
+    const imgSRC = movie.Poster === "N/A" ? "" : movie.Poster;
 
     option.classList.add("dropdown-item");
     option.innerHTML = `
-        <img src="${movie.Poster}"/>
+        <img src="${imgSRC}"/>
         ${movie.Title}
       `;
 
@@ -48,3 +50,9 @@ const onInput = async (e) => {
 };
 
 input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
