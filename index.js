41d4e60ffa7e8bb775f199f0f0d52e47e4a5e49e
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-08-28 11:33:34 +0200

    Finisher autoclose functionality

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
+
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
   resultsWrapper.innerHTML = ``;
 
   dropdown.classList.add("is-active");
@@ -44,6 +50,11 @@ const onInput = async (e) => {
         <img src="${imgSRC}"/>
         ${movie.Title}
       `;
+
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+    });
 
     resultsWrapper.appendChild(option);
   }
